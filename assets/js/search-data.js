693cfa781459d9e547274e2f22c5b460fa074aab
Wuxhu11 <50705046+Wuxhu11@users.noreply.github.com>
2025-08-10 08:46:21 +0000

    Deploying to gh-pages from @ Wuxhu11/wuxhu11.github.io@ab78e9d50bd0ac8b0156addf3fe6241fcfce29ff 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,9 +451,9 @@ ninja.data = [{
           section: "News",},{id: "news-our-paper-on-personalized-federated-learning-is-accepted-to-iccv-2025-in-collaboration-with-dr-qiqi-liu-prof-yaochu-jin-dr-lingjuan-lyu-and-prof-han-yu",
           title: 'Our paper on personalized federated learning is accepted to ICCV 2025, in collaboration...',
           description: "",
-          section: "News",},{id: "projects-an-overview-of-my-research-in-federated-learning",
-          title: 'An Overview of my Research in Federated Learning',
-          description: "with background image",
+          section: "News",},{id: "projects-federated-learning",
+          title: 'Federated Learning',
+          description: "An Overview of my Research",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
