7905aebea788f6a8154c6fbc2b48a494ceb651d5
Wuxhu11 <50705046+Wuxhu11@users.noreply.github.com>
2025-08-10 10:22:52 +0000

    Deploying to gh-pages from @ Wuxhu11/wuxhu11.github.io@b44e3a090e1dbdf333c4565dcb6354bda3c341ca 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -445,7 +445,7 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-two-papers-on-federated-learning-and-vision-language-models-are-accepted-to-icml-2025-in-collaboration-with-prof-han-yu-prof-qicheng-lao-dr-tiantian-he-dr-qiqi-liu-prof-yew-soon-ong-and-prof-yaochu-jin-sparkles-smile",
+            },},{id: "news-two-papers-on-federated-learning-and-vision-language-models-are-accepted-to-icml-2025-in-collaboration-with-prof-han-yu-prof-qicheng-lao-dr-tiantian-he-dr-qiqi-liu-prof-yew-soon-ong-and-prof-yaochu-jin-sparkles",
           title: 'Two papers on federated learning and vision language models are accepted to ICML...',
           description: "",
           section: "News",},{id: "news-our-paper-on-personalized-federated-learning-is-accepted-to-iccv-2025-in-collaboration-with-dr-qiqi-liu-prof-yaochu-jin-dr-lingjuan-lyu-and-prof-han-yu",
