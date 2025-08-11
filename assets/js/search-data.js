34f9c051406634c8e3fa561e6595e734a5912428
Wuxhu11 <50705046+Wuxhu11@users.noreply.github.com>
2025-08-11 01:52:56 +0000

    Deploying to gh-pages from @ Wuxhu11/wuxhu11.github.io@94b716379b3bd74bb7991680bc24ed2a11b9f7cc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-featured-research",
-          title: "featured research",
+        },{id: "nav-research",
+          title: "research",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
