191aac2d51ff47d516770980471c3694918ee1e5
Wuxhu11 <50705046+Wuxhu11@users.noreply.github.com>
2025-08-23 15:16:30 +0000

    Deploying to gh-pages from @ Wuxhu11/wuxhu11.github.io@d4395d940ee6a872a8e57b8da4444afc8e59c2f9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -49,7 +49,7 @@ ninja.data = [{
           description: "Shape a Better World",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/people/";
+            window.location.href = "/services/";
           },
         },{id: "news-we-are-organizing-an-international-workshop-on-federated-learning-with-generative-ai-at-ijcai-2025-together-with-prof-jindong-wang-dr-lingjuan-lyu-dr-dimitrios-dimitriadis-and-prof-han-yu-sparkles",
           title: 'We are organizing an International Workshop on Federated Learning with Generative AI at...',
